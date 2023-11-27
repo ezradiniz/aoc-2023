4808cddbbbf6a624c104815966b789e0b07186c8
Mateus Esdras <linux.esdras@gmail.com>
2023-11-27 19:53:04 -0300

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Advent of Code 2023
+
+My solutions for Advent of Code 2023 to learn [zig](https://ziglang.org/).
+
+https://adventofcode.com/2023/
+
+### Create Day
+```console
+$ ./create.sh <day>
+```
---
+++ create.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+template=$(cat <<EOF
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn main() !void {
+    const input = @embedFile("sample.txt");
+    var it = std.mem.splitSequence(u8, input, "\n");
+    while (it.next()) |line| {
+        if (line.len == 0) continue;
+        print("{s}\n", .{line});
+    }
+}
+EOF
+)
+
+day="$1"
+
+directory=$(printf "day_%02d" "$day")
+
+if [ -d "$directory" ]; then
+    echo "This $directory already exists!"
+    exit 1
+fi
+
+mkdir -p "$directory"
+echo "$template" > "./$directory/main.zig"
+touch "./$directory/sample.txt"
+touch "./$directory/input.txt"
+
+echo "Enjoy day $day!"
