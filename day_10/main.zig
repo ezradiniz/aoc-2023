5a5606454530012df6c97aa3092342b406c74382
Mateus Esdras <linux.esdras@gmail.com>
2023-12-10 21:28:03 -0300

    day_10: refactor

--- day_10/main.zig
+++ day_10/main.zig
@@ -5,192 +5,187 @@ const AutoHashMap = std.AutoHashMap;
 const ArrayList = std.ArrayList;
 const print = std.debug.print;
 
+const inf = std.math.maxInt(i32);
+
 const Pos = struct {
     x: usize,
     y: usize,
 };
 
-const Start = struct {
+const Dir = struct {
     x: i32,
     y: i32,
-    t: []const u8,
 };
 
-fn appendQueue(queue: *ArrayList(Pos), dirs: []const [2]i32, dist: *AutoHashMap(Pos, i32), m: usize, n: usize, x: usize, y: usize) !void {
-    const cur_dist = dist.get(Pos{ .x = x, .y = y }) orelse 0;
-    for (dirs) |d| {
-        const dx = d[0];
-        const dy = d[1];
-        const nx: i32 = @as(i32, @intCast(x)) + dx;
-        const ny: i32 = @as(i32, @intCast(y)) + dy;
-        if (nx < 0 or nx >= m or ny < 0 or ny >= n) {
-            continue;
-        }
-        const new_x = @as(usize, @intCast(nx));
-        const new_y = @as(usize, @intCast(ny));
-        const nxt_dist = dist.get(Pos{ .x = new_x, .y = new_y }) orelse std.math.maxInt(i32);
-        // print("({d},{d}) -> ({d},{d}) | {d} -> {d}\n", .{ x, y, new_x, new_y, cur_dist, nxt_dist });
-        if (nxt_dist > cur_dist + 1) {
-            try queue.append(Pos{ .x = new_x, .y = new_y });
-            try dist.put(Pos{ .x = new_x, .y = new_y }, cur_dist + 1);
-        }
-    }
-}
+const StartDir = struct {
+    dir: Dir,
+    pipes: []const u8,
+};
 
-fn findMaxDist(alloc: Allocator, grid: [][]const u8, start_x: usize, start_y: usize) !i32 {
-    const m = grid.len;
-    const n = grid[0].len;
-    var dist = AutoHashMap(Pos, i32).init(alloc);
-    defer dist.deinit();
-    var queue = ArrayList(Pos).init(alloc);
-    defer queue.deinit();
-    try queue.append(.{ .x = start_x, .y = start_y });
-    try dist.put(Pos{ .x = start_x, .y = start_y }, 0);
-    var i: usize = 0;
-    while (i < queue.items.len) {
-        const len = queue.items.len;
-        while (i < len) : (i += 1) {
-            const pos = queue.items[i];
-            const x = pos.x;
-            const y = pos.y;
-            const cur = grid[x][y];
-            // print("cur: ({d},{d}) - {c}\n", .{ x, y, cur });
-            switch (cur) {
-                '|' => {
-                    const dirs = [_][2]i32{ [2]i32{ 1, 0 }, [2]i32{ -1, 0 } };
-                    try appendQueue(&queue, &dirs, &dist, m, n, x, y);
-                },
-                '-' => {
-                    const dirs = [_][2]i32{ [2]i32{ 0, 1 }, [2]i32{ 0, -1 } };
-                    try appendQueue(&queue, &dirs, &dist, m, n, x, y);
-                },
-                'L' => {
-                    const dirs = [_][2]i32{ [2]i32{ -1, 0 }, [2]i32{ 0, 1 } };
-                    try appendQueue(&queue, &dirs, &dist, m, n, x, y);
-                },
-                'J' => {
-                    const dirs = [_][2]i32{ [2]i32{ -1, 0 }, [2]i32{ 0, -1 } };
-                    try appendQueue(&queue, &dirs, &dist, m, n, x, y);
-                },
-                '7' => {
-                    const dirs = [_][2]i32{ [2]i32{ 1, 0 }, [2]i32{ 0, -1 } };
-                    try appendQueue(&queue, &dirs, &dist, m, n, x, y);
-                },
-                'F' => {
-                    const dirs = [_][2]i32{ [2]i32{ 1, 0 }, [2]i32{ 0, 1 } };
-                    try appendQueue(&queue, &dirs, &dist, m, n, x, y);
-                },
-                'S' => {
-                    const s_dirs = [_]Start{
-                        Start{ .x = -1, .y = 0, .t = "|LJ7F" },
-                        Start{ .x = 1, .y = 0, .t = "|LJ7F" },
-                        Start{ .x = 0, .y = 1, .t = "-LJ7F" },
-                        Start{ .x = 0, .y = -1, .t = "-LJ7F" },
-                    };
-                    for (s_dirs) |d| {
-                        const dx = d.x;
-                        const dy = d.y;
-                        const nx: i32 = @as(i32, @intCast(x)) + dx;
-                        const ny: i32 = @as(i32, @intCast(y)) + dy;
-                        if (nx < 0 or nx >= m or ny < 0 or ny >= n) {
-                            continue;
-                        }
-                        const new_x = @as(usize, @intCast(nx));
-                        const new_y = @as(usize, @intCast(ny));
-                        if (std.mem.indexOfScalar(u8, d.t, grid[new_x][new_y])) |_| {
-                            //const dirs = [_][2]i32{ [2]i32{ -1, 0 }, [2]i32{ 1, 0 } };
-                            // print("{d},{d} - {c}\n", .{ d.x, d.y, grid[new_x][new_y] });
-                            try appendQueue(&queue, &[_][2]i32{[2]i32{ d.x, d.y }}, &dist, m, n, x, y);
-                        }
-                    }
-                },
-                else => unreachable,
+const start_dir = [_]StartDir{
+    StartDir{ .dir = Dir{ .x = -1, .y = 0 }, .pipes = "|LJ7F" },
+    StartDir{ .dir = Dir{ .x = 1, .y = 0 }, .pipes = "|LJ7F" },
+    StartDir{ .dir = Dir{ .x = 0, .y = 1 }, .pipes = "-LJ7F" },
+    StartDir{ .dir = Dir{ .x = 0, .y = -1 }, .pipes = "-LJ7F" },
+};
+
+const pipes_dir = ComptimeStringMap([2]Dir, .{
+    .{ "|", [2]Dir{ Dir{ .x = 1, .y = 0 }, Dir{ .x = -1, .y = 0 } } },
+    .{ "-", [2]Dir{ Dir{ .x = 0, .y = 1 }, Dir{ .x = 0, .y = -1 } } },
+    .{ "L", [2]Dir{ Dir{ .x = -1, .y = 0 }, Dir{ .x = 0, .y = 1 } } },
+    .{ "J", [2]Dir{ Dir{ .x = -1, .y = 0 }, Dir{ .x = 0, .y = -1 } } },
+    .{ "7", [2]Dir{ Dir{ .x = 1, .y = 0 }, Dir{ .x = 0, .y = -1 } } },
+    .{ "F", [2]Dir{ Dir{ .x = 1, .y = 0 }, Dir{ .x = 0, .y = 1 } } },
+});
+
+// TODO: This queue do not free the items on pop
+// TODO: Implement a proper Queue
+const PipeQueue = struct {
+    queue: ArrayList(Pos),
+    dist: AutoHashMap(Pos, i32),
+    idx: usize,
+
+    pub fn init(alloc: Allocator) PipeQueue {
+        return PipeQueue{
+            .queue = ArrayList(Pos).init(alloc),
+            .dist = AutoHashMap(Pos, i32).init(alloc),
+            .idx = 0,
+        };
+    }
+
+    pub fn append(self: *PipeQueue, pos: Pos, new_dist: i32) !void {
+        try self.queue.append(pos);
+        try self.dist.put(pos, new_dist);
+    }
+
+    pub fn popFront(self: *PipeQueue) ?Pos {
+        if (!self.isEmpty()) {
+            const pos = self.queue.items[self.idx];
+            self.idx += 1;
+            return pos;
+        }
+        return null;
+    }
+
+    pub fn getDist(self: *PipeQueue, pos: Pos) i32 {
+        return self.dist.get(pos) orelse inf;
+    }
+
+    pub fn getMaxDist(self: *PipeQueue) i32 {
+        // TODO: Set the max distance in .append
+        var max_dist: i32 = 0;
+        var dist_it = self.dist.valueIterator();
+        while (dist_it.next()) |d| {
+            max_dist = @max(max_dist, d.*);
+        }
+        return max_dist;
+    }
+
+    pub fn deinit(self: *PipeQueue) void {
+        self.queue.deinit();
+        self.dist.deinit();
+    }
+
+    pub fn isEmpty(self: *PipeQueue) bool {
+        return self.idx >= self.queue.items.len;
+    }
+};
+
+fn findStarPos(grid: [][]const u8) ?Pos {
+    for (grid, 0..) |row, i| {
+        for (row, 0..) |val, j| {
+            if (val == 'S') {
+                return Pos{ .x = i, .y = j };
             }
         }
     }
-    var max_dist: i32 = 0;
-    var dist_it = dist.valueIterator();
-    while (dist_it.next()) |d| {
-        max_dist = @max(max_dist, d.*);
-    }
-    return max_dist;
+    return null;
 }
 
-fn countEnclosedTiles(alloc: Allocator, grid: [][]const u8, start_x: usize, start_y: usize) !i32 {
+// TODO: Avoid unnecessary casting
+fn calcNextPos(pos: Pos, dir: Dir, m: usize, n: usize) ?Pos {
+    const nx: i32 = @as(i32, @intCast(pos.x)) + dir.x;
+    const ny: i32 = @as(i32, @intCast(pos.y)) + dir.y;
+    if (nx < 0 or nx >= m or ny < 0 or ny >= n) {
+        return null;
+    }
+    const new_x = @as(usize, @intCast(nx));
+    const new_y = @as(usize, @intCast(ny));
+    return Pos{ .x = new_x, .y = new_y };
+}
+
+fn findMaxDist(alloc: Allocator, grid: [][]const u8) !i32 {
     const m = grid.len;
     const n = grid[0].len;
-    var pipes = AutoHashMap(Pos, void).init(alloc);
-    defer pipes.deinit();
-    var dist = AutoHashMap(Pos, i32).init(alloc);
-    defer dist.deinit();
-    var queue = ArrayList(Pos).init(alloc);
-    defer queue.deinit();
-    try queue.append(.{ .x = start_x, .y = start_y });
-    try dist.put(Pos{ .x = start_x, .y = start_y }, 0);
-    var i: usize = 0;
-    while (i < queue.items.len) {
-        const len = queue.items.len;
-        while (i < len) : (i += 1) {
-            const pos = queue.items[i];
-            const x = pos.x;
-            const y = pos.y;
-            const cur = grid[x][y];
-            try pipes.put(pos, {});
-            // print("cur: ({d},{d}) - {c}\n", .{ x, y, cur });
-            switch (cur) {
-                '|' => {
-                    const dirs = [_][2]i32{ [2]i32{ 1, 0 }, [2]i32{ -1, 0 } };
-                    try appendQueue(&queue, &dirs, &dist, m, n, x, y);
-                },
-                '-' => {
-                    const dirs = [_][2]i32{ [2]i32{ 0, 1 }, [2]i32{ 0, -1 } };
-                    try appendQueue(&queue, &dirs, &dist, m, n, x, y);
-                },
-                'L' => {
-                    const dirs = [_][2]i32{ [2]i32{ -1, 0 }, [2]i32{ 0, 1 } };
-                    try appendQueue(&queue, &dirs, &dist, m, n, x, y);
-                },
-                'J' => {
-                    const dirs = [_][2]i32{ [2]i32{ -1, 0 }, [2]i32{ 0, -1 } };
-                    try appendQueue(&queue, &dirs, &dist, m, n, x, y);
-                },
-                '7' => {
-                    const dirs = [_][2]i32{ [2]i32{ 1, 0 }, [2]i32{ 0, -1 } };
-                    try appendQueue(&queue, &dirs, &dist, m, n, x, y);
-                },
-                'F' => {
-                    const dirs = [_][2]i32{ [2]i32{ 1, 0 }, [2]i32{ 0, 1 } };
-                    try appendQueue(&queue, &dirs, &dist, m, n, x, y);
-                },
-                'S' => {
-                    const s_dirs = [_]Start{
-                        Start{ .x = -1, .y = 0, .t = "|LJ7F" },
-                        Start{ .x = 1, .y = 0, .t = "|LJ7F" },
-                        Start{ .x = 0, .y = 1, .t = "-LJ7F" },
-                        Start{ .x = 0, .y = -1, .t = "-LJ7F" },
-                    };
-                    for (s_dirs) |d| {
-                        const dx = d.x;
-                        const dy = d.y;
-                        const nx: i32 = @as(i32, @intCast(x)) + dx;
-                        const ny: i32 = @as(i32, @intCast(y)) + dy;
-                        if (nx < 0 or nx >= m or ny < 0 or ny >= n) {
-                            continue;
-                        }
-                        const new_x = @as(usize, @intCast(nx));
-                        const new_y = @as(usize, @intCast(ny));
-                        if (std.mem.indexOfScalar(u8, d.t, grid[new_x][new_y])) |_| {
-                            //const dirs = [_][2]i32{ [2]i32{ -1, 0 }, [2]i32{ 1, 0 } };
-                            // print("{d},{d} - {c}\n", .{ d.x, d.y, grid[new_x][new_y] });
-                            try appendQueue(&queue, &[_][2]i32{[2]i32{ d.x, d.y }}, &dist, m, n, x, y);
-                        }
+    var pipe_queue = PipeQueue.init(alloc);
+    defer pipe_queue.deinit();
+    const start_pos = findStarPos(grid).?;
+    try pipe_queue.append(start_pos, 0);
+    while (!pipe_queue.isEmpty()) {
+        const pos = pipe_queue.popFront().?;
+        const cur_dist = pipe_queue.getDist(pos);
+        const tile = grid[pos.x][pos.y];
+        const tile_key = [_]u8{tile};
+        if (pipes_dir.has(&tile_key)) {
+            const dirs = pipes_dir.get(&tile_key).?;
+            for (dirs) |dir| {
+                if (calcNextPos(pos, dir, m, n)) |new_pos| {
+                    const new_dist = pipe_queue.getDist(new_pos);
+                    if (new_dist > cur_dist + 1) {
+                        try pipe_queue.append(new_pos, cur_dist + 1);
                     }
-                },
-                else => unreachable,
+                }
+            }
+        } else if (tile == 'S') {
+            for (start_dir) |sd| {
+                if (calcNextPos(pos, sd.dir, m, n)) |new_pos| {
+                    const new_tile = grid[new_pos.x][new_pos.y];
+                    if (std.mem.indexOfScalar(u8, sd.pipes, new_tile)) |_| {
+                        try pipe_queue.append(new_pos, 1);
+                    }
+                }
             }
         }
     }
+    return pipe_queue.getMaxDist();
+}
 
+fn countEnclosedTiles(alloc: Allocator, grid: [][]const u8) !i32 {
+    const m = grid.len;
+    const n = grid[0].len;
+    var pipe_queue = PipeQueue.init(alloc);
+    defer pipe_queue.deinit();
+    const start_pos = findStarPos(grid).?;
+    try pipe_queue.append(start_pos, 0);
+    var pipes_set = AutoHashMap(Pos, void).init(alloc);
+    defer pipes_set.deinit();
+    while (!pipe_queue.isEmpty()) {
+        const pos = pipe_queue.popFront().?;
+        const cur_dist = pipe_queue.getDist(pos);
+        const tile = grid[pos.x][pos.y];
+        const tile_key = [_]u8{tile};
+        try pipes_set.put(pos, {});
+        if (pipes_dir.has(&tile_key)) {
+            const dirs = pipes_dir.get(&tile_key).?;
+            for (dirs) |dir| {
+                if (calcNextPos(pos, dir, m, n)) |new_pos| {
+                    const new_dist = pipe_queue.getDist(new_pos);
+                    if (new_dist > cur_dist + 1) {
+                        try pipe_queue.append(new_pos, cur_dist + 1);
+                    }
+                }
+            }
+        } else if (tile == 'S') {
+            for (start_dir) |sd| {
+                if (calcNextPos(pos, sd.dir, m, n)) |new_pos| {
+                    const new_tile = grid[new_pos.x][new_pos.y];
+                    if (std.mem.indexOfScalar(u8, sd.pipes, new_tile)) |_| {
+                        try pipe_queue.append(new_pos, 1);
+                    }
+                }
+            }
+        }
+    }
     // Reference: https://en.wikipedia.org/wiki/Point_in_polygon
     var result: i32 = 0;
     for (0..m) |x| {
@@ -198,19 +193,21 @@ fn countEnclosedTiles(alloc: Allocator, grid: [][]const u8, start_x: usize, star
         var count: i32 = 0;
         var is_inside: bool = false;
         for (0..n) |y| {
-            if (pipes.contains(Pos{ .x = x, .y = y })) {
-                const cur = grid[x][y];
+            const cur = grid[x][y];
+            if (pipes_set.contains(Pos{ .x = x, .y = y })) { // Main
+                // NOTE: i.e line ---
                 if (cur == '-') continue;
-                if (!((cur == 'J' and prev == 'F') or (cur == '7' and prev == 'L'))) {
+                // NOTE: i.e ] [
+                if (!((prev == 'F' and cur == 'J') or (prev == 'L' and cur == '7'))) {
                     count += 1;
                 }
-                prev = cur;
                 is_inside = @mod(count, 2) == 1;
-            } else {
+            } else { // Other Tiles
                 if (is_inside) {
                     result += 1;
                 }
             }
+            prev = cur;
         }
     }
     return result;
@@ -226,24 +223,16 @@ pub fn main() !void {
     var grid = ArrayList([]const u8).init(alloc);
     defer grid.deinit();
 
-    var start_x: usize = undefined;
-    var start_y: usize = undefined;
-
     var it = std.mem.tokenizeAny(u8, input, "\n");
-    var i: usize = 0;
-    while (it.next()) |line| : (i += 1) {
+    while (it.next()) |line| {
         try grid.append(line);
-        for (line, 0..) |ch, j| {
-            if (ch == 'S') {
-                start_x = i;
-                start_y = j;
-            }
-        }
     }
 
-    const part1 = try findMaxDist(alloc, grid.items, start_x, start_y);
-    const part2 = try countEnclosedTiles(alloc, grid.items, start_x, start_y);
+    const part1 = try findMaxDist(alloc, grid.items);
+    const part2 = try countEnclosedTiles(alloc, grid.items);
 
     print("part1: {d}\n", .{part1});
     print("part2: {d}\n", .{part2});
 }
+
+// TODO: Add tests
